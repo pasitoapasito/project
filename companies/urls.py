1ff18e3f63c2ee18bc89412da4b75b951290187c
DGK <rlaehdrb0923@gmail.com>
2022-06-19 16:51:52 +0900

    Create : companies app API

---
+++ companies/urls.py
@@ -0,0 +1,9 @@
+from django.urls     import path
+from companies.views import JobPositionView, JobPositionListView, JobPositionDetailView
+
+urlpatterns = [
+    path('/positions', JobPositionListView.as_view()),
+    path('/position/<int:position_id>', JobPositionDetailView.as_view()),
+    path('/position', JobPositionView.as_view()),
+]
+
